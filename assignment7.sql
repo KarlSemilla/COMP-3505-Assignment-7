69c0a3c8f557d105449d9e0aa2d924e611d45b1c
KarlSemilla <karlsemilla1@gmail.com>
2020-03-08 20:27:11 -0600

    initial push

---
+++ assignment7.sql
@@ -0,0 +1,4 @@
+sudo docker run -p 8080:8080 --name webgoat --rm -it -v ~/webgoatdata:/home/webgoat/.webgoat-8.0-8088465 webgoat/webgoat-8.0:latest
+
+1. select department from employees where last_name = 'Franco';
+2. update employees set department = 'Sales' where userid = '89762'; 
---
+++ readme.md
@@ -0,0 +1,3 @@
+# COMP 3505 Assignment 7
+
+This assignment tested SQL injection on the WebGoat application.
